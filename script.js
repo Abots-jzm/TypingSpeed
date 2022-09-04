21b550bce47c260912ed4a5fa942233d3ed4b24f
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-09-04 21:53:15 +0100

    Results and final changes

--- script.js
+++ script.js
@@ -12,36 +12,50 @@ const mainScreen = document.querySelector(".test-screen");
 const quotesBox = document.querySelector(".quotes-box");
 const inputField = document.querySelector(".hidden-input input");
 const spinner1 = document.querySelector(".test-screen .loading-spinner");
+const countdownTimer = document.querySelector(".countdown");
+const speedDisplay = document.querySelector(".speed");
+const accuracyDisplay = document.querySelector(".accuracy");
+const test = document.querySelector(".test");
+const results = document.querySelector(".test-result");
+const restartBtn = document.querySelector(".restart");
 
-const activeQuotes = [];
+let data;
 
+let activeQuotes = [];
 let quoteText = "";
+let timer;
+let time = 30;
+let countdownStarted = false;
 let typingAnimationCounter = 0;
 
 async function fetchQuotes() {
 	try {
 		const response = await fetch(API_URL);
 		if (!response?.ok) throw new Error("unable to fetch data.");
-		const data = await response.json();
+		data = await response.json();
 
-		let noOfQuotes = data.length;
-		let noOfWords = 0;
-		while (noOfWords < MINIMUM_NO_OF_WORDS) {
-			const random = Math.floor(Math.random() * noOfQuotes) + 1;
-			const quote = data[random].text;
-
-			if (activeQuotes.includes(quote)) continue;
-
-			activeQuotes.push(quote);
-			noOfWords += quote.split(" ").length;
-			noOfQuotes--;
-		}
-		displayQuotes();
+		setActiveQuotes(data);
 	} catch (e) {
 		throw e;
 	} finally {
 		spinner1.classList.add("hidden");
 	}
+}
+
+function setActiveQuotes(data) {
+	let noOfQuotes = data.length;
+	let noOfWords = 0;
+	while (noOfWords < MINIMUM_NO_OF_WORDS) {
+		const random = Math.floor(Math.random() * noOfQuotes) + 1;
+		const quote = data[random].text;
+
+		if (activeQuotes.includes(quote)) continue;
+
+		activeQuotes.push(quote);
+		noOfWords += quote.split(" ").length;
+		noOfQuotes--;
+	}
+	displayQuotes();
 }
 
 function typingAnimation() {
@@ -74,13 +88,71 @@ function displayQuotes() {
 	quotesBox.insertAdjacentHTML("beforeend", finalHTML);
 }
 
+function restart() {
+	inputField.value = "";
+	inputField.focus();
+	activeQuotes = [];
+	quoteText = "";
+	countdownStarted = false;
+	time = 30;
+	clearInterval(timer);
+	countdownTimer.textContent = "30";
+	countdownTimer.classList.remove("started");
+	quotesBox.innerHTML = `<span class="letter"><span class="cursor">|</span></span>`;
+	setActiveQuotes(data);
+
+	firstScreen.classList.add("hidden");
+	mainScreen.classList.remove("hidden");
+	results.classList.add("hidden");
+	test.classList.remove("hidden");
+	quotesBox.scrollTop = 0;
+}
+
 function checkForScroll(cursor) {
 	const cursorCoords = cursor.getBoundingClientRect();
 	const quotesCoords = quotesBox.getBoundingClientRect();
 	if (cursorCoords.y > quotesCoords.y + quotesCoords.height / 2) quotesBox.scrollTop += quotesCoords.height / 4;
 }
 
+function displayScores(speed, accuracy) {
+	speedDisplay.firstChild.nodeValue = speed;
+	accuracyDisplay.firstChild.nodeValue = accuracy;
+	test.classList.add("hidden");
+	results.classList.remove("hidden");
+}
+
+function calculateFinalSpeed() {
+	const input = inputField.value;
+	const expected = quoteText;
+	let correct = 0;
+	let total = input.length;
+
+	[...input].forEach((v, i) => v === expected[i] && correct++);
+	const speed = ((correct / 5) * 2).toFixed(0);
+	const accuracy = ((correct / total) * 100).toFixed(0);
+
+	displayScores(speed, accuracy);
+}
+
+function countdown() {
+	countdownTimer.classList.add("started");
+	timer = setInterval(function () {
+		if (time <= 0) {
+			calculateFinalSpeed();
+			clearInterval(timer);
+			return;
+		}
+
+		time--;
+		countdownTimer.textContent = time.toString();
+	}, 1000);
+
+	countdownStarted = true;
+}
+
 function validateInput(e) {
+	if (!countdownStarted) countdown();
+
 	let i = 0;
 	quoteText.split(" ").forEach((word, wordIndex) => {
 		const input = e.target.value;
@@ -130,6 +202,8 @@ inputField.addEventListener("input", validateInput);
 inputField.addEventListener("blur", () => document.querySelectorAll(".cursor").forEach((v) => v.classList.add("hidden")));
 inputField.addEventListener("focus", () => document.querySelectorAll(".cursor").forEach((v) => v.classList.remove("hidden")));
 inputField.addEventListener("keydown", (e) => e.key.startsWith("Arrow") && e.preventDefault());
+
+restartBtn.addEventListener("click", restart);
 
 //START
 
